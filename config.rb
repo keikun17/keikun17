ed9acf333647bd19252dbf08f4a605853c1f6c0d
Buddy Magsipoc <keikun17@gmail.com>
2016-01-22 16:58:00 +0800

    Minify + relative links。

--- config.rb
+++ config.rb
@@ -35,10 +35,13 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :relative_assets
+  set :relative_links, true
 end
 
 activate :autoprefixer
